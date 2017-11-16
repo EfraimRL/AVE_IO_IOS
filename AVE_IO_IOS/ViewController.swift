b73dd75944031bd39dfa6e2eff30b14730d8d599
EfraimRL <32856906+EfraimRL@users.noreply.github.com>
2017-11-16 15:02:13 -0600

    Initial Commit

---
+++ AVE_IO_IOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AVE_IO_IOS
+//
+//  Created by MAC 2 on 16/11/17.
+//  Copyright © 2017 EVA Transporte. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
